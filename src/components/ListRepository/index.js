d02f9c20e9eea2d12d0aaabe0906e1b57624b832
Giselle Ferreira <giselles629@gmail.com>
2023-10-22 04:13:40 -0300

    feat: Renderização da lista de repositório

--- src/components/ListRepository/index.js
+++ src/components/ListRepository/index.js
@@ -1,19 +1,56 @@
 // ** Import React
-import React from 'react'
+import React from "react";
 
 // ** Import Library Material React Native
-import { List } from 'react-native-paper';
+import { FlatList, StyleSheet, View } from "react-native";
+import { Card, Text, Button, Avatar } from "react-native-paper";
 
-export default function ListRepository() {
-  const [expanded, setExpanded] = React.useState(true);
-
-  const handlePress = () => setExpanded(!expanded);
-
+export default function ListRepository({ data }) {
   return (
-    <List.Item
-    title="First Item"
-    description="Item description"
-    left={props => <List.Icon {...props} icon="folder" />}
-  />
+    <View style={styles.container}>
+      <FlatList
+        data={data.items}
+        keyExtractor={({ id }) => id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            style={styles.cardContainer}
+            onPress={() => console.log("card")}
+          >
+            <Card.Title
+              title={item.full_name}
+              titleStyle={styles.cardTitle}
+              subtitle={item.name}
+              subtitleStyle={styles.cardSubtitle}
+              left={(props) => (
+                <Avatar.Image
+                  size={45}
+                  source={{ uri: item.owner.avatar_url }}
+                />
+              )}
+            />
+            <Card.Content >
+              <Text variant="titleLarge">{item.description}</Text>
+            </Card.Content>
+          </Card>
+        )}
+      />
+    </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,    
+  },
+  cardContainer: {
+    borderRadius: 10,
+    marginHorizontal: 16,
+    marginVertical: 5,
+  },
+  cardTitle:{
+    fontSize: 15
+  },
+  cardSubtitle:{
+    paddingBottom:7,
+  },
+});
